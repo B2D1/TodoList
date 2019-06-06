697a4c23f9b41d2072655e15b22178d5187cdc3f
B2D1 <baobangdong@aliyun.com>
2019-06-06 14:50:02 +0800

    refactor: eject

---
+++ src/app.test.tsx
@@ -0,0 +1,11 @@
+test('two plus two', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  // toBe and toEqual are equivalent for numbers
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+});
