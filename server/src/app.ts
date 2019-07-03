1d7c80dca9d522d17544c3a7ffe512184a017b75
B2D1 <baobangdong@aliyun.com>
2019-07-03 20:46:57 +0800

    feat: add https

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,22 +1,32 @@
+import * as fs from 'fs';
+import * as https from 'https';
 import * as Koa from 'koa';
 import * as bodyParser from 'koa-bodyparser';
 import * as cors from 'koa2-cors';
+import * as path from 'path';
 
 import Config from './config';
 import initDB from './db';
 import todoRouter from './routes/todo';
 import userRouter from './routes/user';
 
-const app = new Koa();
+const app = new Koa() as any;
 
+const privateKey = fs.readFileSync(path.join(__dirname, './certificate/2265242_baobangdong.cn.key'), 'utf8');
+const certificate = fs.readFileSync(path.join(__dirname, './certificate/2265242_baobangdong.cn.pem'), 'utf8');
+const credentials = {
+  key: privateKey,
+  cert: certificate
+};
+const httpsServer = https.createServer(credentials, app);
 app
   .use(cors())
   .use(bodyParser())
   .use(userRouter.routes())
   .use(todoRouter.routes());
 
-app.listen(Config.PORT, () => {
-  console.log(`Server is running on: http://localhost:${Config.PORT}`);
+httpsServer.listen(Config.PORT, () => {
+  console.log(`Server is running on: https://localhost:${Config.PORT}`);
 });
 
 initDB(Config.DB_URL);
