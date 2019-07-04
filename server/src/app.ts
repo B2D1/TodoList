1c38695cae66fe5e6d9a2f3e5e9578508429c751
B2D1 <baobangdong@aliyun.com>
2019-07-04 11:02:28 +0800

    fix: https

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,6 @@
-import * as fs from 'fs';
-import * as https from 'https';
 import * as Koa from 'koa';
 import * as bodyParser from 'koa-bodyparser';
 import * as cors from 'koa2-cors';
-import * as path from 'path';
 
 import Config from './config';
 import initDB from './db';
@@ -12,21 +9,21 @@ import userRouter from './routes/user';
 
 const app = new Koa();
 
-const privateKey = fs.readFileSync(path.join(__dirname, './certificate/2265242_baobangdong.cn.key'), 'utf8');
-const certificate = fs.readFileSync(path.join(__dirname, './certificate/2265242_baobangdong.cn.pem'), 'utf8');
-const credentials = {
-  key: privateKey,
-  cert: certificate
-};
-const httpsServer = https.createServer(credentials, app.callback());
+// const privateKey = fs.readFileSync(path.join(__dirname, './certificate/2265242_baobangdong.cn.key'), 'utf8');
+// const certificate = fs.readFileSync(path.join(__dirname, './certificate/2265242_baobangdong.cn.pem'), 'utf8');
+// const credentials = {
+//   key: privateKey,
+//   cert: certificate
+// };
+// const httpsServer = https.createServer(credentials, app.callback());
 app
   .use(cors())
   .use(bodyParser())
   .use(userRouter.routes())
   .use(todoRouter.routes());
 
-httpsServer.listen(Config.PORT, () => {
-  console.log(`Server is running on: https://localhost:${Config.PORT}`);
+app.listen(Config.PORT, () => {
+  console.log(`Server is running on: http://localhost:${Config.PORT}`);
 });
 
 initDB(Config.DB_URL);
