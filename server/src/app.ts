54f66b6686e6b7cae973f16a6f6b87659c7c1c35
B2D1 <baobangdong@aliyun.com>
2019-09-03 15:21:43 +0800

    feat: node https

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,46 @@
 import * as Koa from 'koa';
 import * as bodyParser from 'koa-bodyparser';
 import * as cors from 'koa2-cors';
-
+import * as fs from 'fs';
+import * as path from 'path';
+import * as https from 'https';
 import Config from './config';
 import initDB from './db';
 import todoRouter from './routes/todo';
 import userRouter from './routes/user';
-
-const app = new Koa();
-
-app.use(cors())
+var config = {
+    domain: 'baobangdong.cn',
+    https: {
+        port: 4000,
+        options: {
+            key: fs
+                .readFileSync(
+                    path.resolve('../certificate/2265242_baobangdong.cn.key'),
+                    'utf8'
+                )
+                .toString(),
+            cert: fs
+                .readFileSync(
+                    path.resolve('../certificate2265242_baobangdong.cn.pem'),
+                    'utf8'
+                )
+                .toString(),
+        },
+    },
+};
+const server = new Koa();
+server
+    .use(cors())
     .use(bodyParser())
     .use(userRouter.routes())
     .use(todoRouter.routes());
 
-app.listen(Config.PORT, () => {
-    console.log(`Server is running on: http://localhost:${Config.PORT}`);
-});
+const serverCallback = server.callback();
 
+var httpsServer = https.createServer(config.https.options, serverCallback);
+httpsServer.listen(config.https.port, () => {
+    console.log(
+        `HTTPS server OK: http://${config.domain}:${config.https.port}`
+    );
+});
 initDB(Config.__MONGO_URI__);
