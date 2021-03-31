0d6aa2b1cb13c8700070d30942758cd79e43d419
baobangdong <baobangdong@bytedance.com>
2021-03-31 14:20:15 +0800

    add deploy branch
    Change-Id: I8508788d616ee6091c1bbf915b55bd42136e1ab2

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,47 +2,21 @@ import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import cors from 'koa2-cors';
 
-import fs from 'fs';
-import https from 'https';
-import path from 'path';
-
 import Config from './config';
 import connectDB from './db';
 import todoRouter from './routes/todo';
 import userRouter from './routes/user';
 
+const app = new Koa();
+
 connectDB(Config.MONGODB_URI);
 
-var config = {
-  domain: 'baobangdong.cn',
-  https: {
-    port: 5000,
-    options: {
-      key: fs
-        .readFileSync(
-          path.resolve(__dirname, './cert/5408545_baobangdong.cn.key'),
-          'utf8'
-        )
-        .toString(),
-      cert: fs
-        .readFileSync(
-          path.resolve(__dirname, './cert/5408545_baobangdong.cn.pem'),
-          'utf8'
-        )
-        .toString(),
-    },
-  },
-};
-const server = new Koa();
-server
+app
   .use(cors())
   .use(bodyParser())
   .use(userRouter.routes())
   .use(todoRouter.routes());
 
-const serverCallback = server.callback();
-
-var httpsServer = https.createServer(config.https.options, serverCallback);
-httpsServer.listen(config.https.port, () => {
-  console.log(`HTTPS server OK: https://${config.domain}:${config.https.port}`);
+app.listen(Config.PORT, () => {
+  console.log(`server starts successful: http://localhost:${Config.PORT}`);
 });
